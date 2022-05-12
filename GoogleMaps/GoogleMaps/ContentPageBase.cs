6d4fef1213d91016d734df9db9c0a111a22feb94
olivertamryn <4270497@myuwc.ac.za>
2022-05-12 13:09:40 +0200

    Add project files.

---
+++ GoogleMaps/GoogleMaps/ContentPageBase.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace GoogleMaps
+{
+    [XamlCompilation(XamlCompilationOptions.Compile), XamlFilePath("ContentPage.xaml")]
+    public class ContentPageBase
+    {
+    }
+}
---
+++ GoogleMaps/GoogleMaps/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoogleMaps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Delay(5000);
+            await Navigation.PushAsync(new mapsPage());
+        }
+    }  
+}
